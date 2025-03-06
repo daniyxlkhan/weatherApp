62e0e486d60c37bedde0bec20b8d8c4f311b9dd4
Javantax <79591148+Javantax@users.noreply.github.com>
2025-03-06 07:45:58 -0500

    Add get weather report from api fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+
+let city = "london";
+const API_KEY = "SYRZ7YCFF93B8SKQ87KZHEYRU";
+
+// fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(city)}?unitGroup=us&key=SYRZ7YCFF93B8SKQ87KZHEYRU&contentType=json`, {mode: 'cors'})
+// .then(function(response) {
+//     return response.json();
+// })
+// .then(function(result) {
+//     console.log(result);
+// })
+
+async function getWeatherFromAPI() {
+    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(city)}?unitGroup=us&key=${encodeURIComponent(API_KEY)}&contentType=json`
+    try{
+        let response = await fetch(url, {mode: 'cors'});
+        let result = await response.json();
+        return result;
+    } catch (error) {
+        console.log("ERROR OCCURED: ", error);
+    }
+}
+
+
+getWeatherFromAPI().then((result) => console.log(result));
