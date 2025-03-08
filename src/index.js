3d62f2e856ddda3e4ff41fc4713eda142968b74c
Javantax <79591148+Javantax@users.noreply.github.com>
2025-03-08 15:41:40 -0500

    Add input box to search weather

--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
 import "./styles.css";
+import { getCurrentWeather } from "./weather";
 
-let city = "london";
-const API_KEY = "SYRZ7YCFF93B8SKQ87KZHEYRU";
+const form = document.getElementById("form");
 
-// fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(city)}?unitGroup=us&key=SYRZ7YCFF93B8SKQ87KZHEYRU&contentType=json`, {mode: 'cors'})
-// .then(function(response) {
-//     return response.json();
-// })
-// .then(function(result) {
-//     console.log(result);
-// })
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-async function getWeatherFromAPI() {
-    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(city)}?unitGroup=us&key=${encodeURIComponent(API_KEY)}&contentType=json`
-    try{
-        let response = await fetch(url, {mode: 'cors'});
-        let result = await response.json();
-        return result;
-    } catch (error) {
-        console.log("ERROR OCCURED: ", error);
-    }
-}
+    const location = document.getElementById("location-input").value;
 
+    getCurrentWeather(location)
+        .then(weather => {
+            if (weather) {
+                console.log(weather);
+            } else {
+                console.error("Given location not found!");
+            }
+        });
+});
 
-getWeatherFromAPI().then((result) => console.log(result));
