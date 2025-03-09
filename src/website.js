2e0ea7979b2f3ff12b330a12dbf936cc34278fd7
Javantax <79591148+Javantax@users.noreply.github.com>
2025-03-08 23:10:46 -0500

    Add search button and styling for smaller devices

--- src/website.js
+++ src/website.js
@@ -2,11 +2,24 @@ import {getCurrentWeather} from "./getWeather";
 
 export function searchLocation() {
     const form = document.getElementById("form");
+    const searchIcon = document.querySelector(".search-icon");
 
+    // if user clicks on search icon
+    searchIcon.addEventListener("click", showWeather);
+
+    // if user presses enter on the search
     form.addEventListener("submit", (event) => {
         event.preventDefault();
+        showWeather();
+    });
 
-        const location = document.getElementById("location-input").value;
+    function showWeather() {
+        const location = document.getElementById("location-input").value.trim();
+
+        if (!location) {
+            console.warn("Please enter a location!");
+            return;
+        }
 
         getCurrentWeather(location)
             .then(weather => {
@@ -16,5 +29,7 @@ export function searchLocation() {
                     console.error("Given location not found!");
                 }
             });
-    });
+    }
 }
+
+
